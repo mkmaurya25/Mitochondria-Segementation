fe2abbaaaff3d2c1a7543525d6bc16179f8f177f
mkmaurya25 <mkmaurya@outlook.com>
2021-05-06 15:09:59 +0530

    First commit

---
+++ U_Net_jaccard.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 18:42:27 2021
+
+@author: M K Maurya
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from skimage import io
+import os
+import cv2
+from PIL import Image
+from tensorflow.keras.utils import normalize
+
+print("The dir is", os.getcwd())
+
+os.chdir("C:\\Users\\M K Maurya\\Data Science\\Image Processing\\Mitochondria")
+print("The current dir is: ", os.getcwd())
+
+image_directory = 'data/train_imgs/'
+mask_directory = 'data/train_masks/'
+
+SIZE = 256
+image_dataset = []
+mask_dataset = []
+
+images = os.listdir(image_directory)
+for i, image_name in enumerate(images):    #Remember enumerate method adds a counter and returns the enumerate object
+    if (image_name.split('.')[1] == 'png'):
+        #print(image_directory+image_name)
+        image = cv2.imread(image_directory+image_name, 0)
+        image = Image.fromarray(image)
+        image = image.resize((SIZE, SIZE))
+        image_dataset.append(np.array(image))
+        
+        
+#Iterate through all images in Uninfected folder, resize to 64 x 64
+#Then save into the same numpy array 'dataset' but with label 1
+
+masks = os.listdir(mask_directory)
+for i, image_name in enumerate(masks):
+    if (image_name.split('.')[1] == 'png'):
+        image = cv2.imread(mask_directory+image_name, 0)
+        image = Image.fromarray(image)
+        image = image.resize((SIZE, SIZE))
+        mask_dataset.append(np.array(image))
+        
+#Normalize images
+image_dataset = np.expand_dims(normalize(np.array(image_dataset), axis=1),3)
+#D not normalize masks, just rescale to 0 to 1.
+mask_dataset = np.expand_dims((np.array(mask_dataset)),3) /255.
+
+X = image_dataset[200:350]
+y = mask_dataset[200:350]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.10, random_state = 0)
+
+X_train_quick_test, X_test_quick_test, y_train_quick_test, y_test_quick_test = train_test_split(X_train, y_train, test_size = 0.9, random_state = 0)
+
+import random
+image_number = random.randint(0, len(X_train_quick_test))
+plt.figure(figsize=(12, 6))
+plt.subplot(121)
+plt.imshow(np.reshape(X_train_quick_test[image_number], (256, 256)), cmap='gray')
+plt.subplot(122)
+plt.imshow(np.reshape(y_train_quick_test[image_number], (256, 256)), cmap='gray')
+plt.show()
+
+from simple_unet_model import simple_unet_model 
+from simple_unet_model_with_jacard import simple_unet_model_with_jacard 
+
+def get_jacard_model():
+    return simple_unet_model_with_jacard(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+
+model_jacard = get_jacard_model()
+
+IMG_HEIGHT = image_dataset.shape[1]
+IMG_WIDTH  = image_dataset.shape[2]
+IMG_CHANNELS = image_dataset.shape[3]
+def get_standard_model():
+    return simple_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+
+model_standard = get_standard_model()
+
+history_standard = model_standard.fit(X_train, y_train, 
+                    batch_size = 16, 
+                    verbose=1, 
+                    epochs=30, 
+                    validation_data=(X_test, y_test), 
+                    shuffle=False)
+model_standard.save('mitochondria_standard.hdf5')
+
+_, acc = model_standard.evaluate(X_test, y_test)
+print("Accuracy of Standard Model is = ", (acc * 100.0), "%")
+
+###
+#plot the training and validation accuracy and loss at each epoch
+loss = history_standard.history['loss']
+val_loss = history_standard.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+accuracy = history_standard.history['acc']
+#acc = history.history['accuracy']
+val_acc = history_standard.history['val_acc']
+#val_acc = history.history['val_accuracy']
